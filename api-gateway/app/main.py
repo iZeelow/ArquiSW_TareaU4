22c15025cd013b499dcc154ab9c43cd6e900ca55
juanpabloLM <juan.labbe@usm.cl>
2023-10-02 21:28:55 -0300

    V0.6.3

--- api-gateway/app/main.py
+++ api-gateway/app/main.py
@@ -14,17 +14,16 @@ from graphql.type import GraphQLResolveInfo
 
 from starlette.middleware.cors import CORSMiddleware
 
-
 type_defs = load_schema_from_path("./app/schema.graphql")
 
 query = QueryType()
 mutation = MutationType()
 
-user= ObjectType("User")
+user = ObjectType("User")
 
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(name)s:%(message)s"
+)
 
 
 @query.field("getUser")
@@ -34,50 +33,104 @@ def resolve_get_user(obj, resolve_info: GraphQLResolveInfo, id):
     if response.status_code == 200:
         return response.json()
 
-@query.field("getAllUsers")
-def resolve_get_all_users(obj, resolve_info: GraphQLResolveInfo):
+
+@query.field("listUsers")
+def resolve_list_users(obj, resolve_info: GraphQLResolveInfo):
+    # Make it slow
+    time.sleep(3)
+
     response = requests.get(f"http://tarea_u4_service_users/users")
 
     if response.status_code == 200:
         return response.json()
 
+
 @mutation.field("createUser")
-def resolve_create_user(obj, resolve_info: GraphQLResolveInfo, name, username, password,email=None,admin=None,phone_number=None):
-    payload = dict(name=name,
-                   username=username,
-                   pasword=password)
-    if email is not None:
-        payload["email"] = email
-    if admin is not None:
-        payload["admin"] = admin
-    if phone_number is not None:
-        payload["phone_number"] = phone_number
+def resolve_create_user(
+    obj,
+    resolve_info: GraphQLResolveInfo,
+    id,
+    name,
+    username,
+    password,
+    email,
+    admin,
+    phone_number,
+    ad
+):
+    payload = dict(
+        id=id,
+        name=name,
+        username=username,
+        password=password,
+        email=email,
+        admin=admin,
+        phone_number=phone_number,
+        ad=ad
+    )
 
     return requests.post(f"http://tarea_u4_service_users/users", json=payload).json()
 
 @mutation.field("updateUser")
-def resolve_update_user(obj, resolve_info: GraphQLResolveInfo, id, name=None,username=None , password=None, email=None,admin=None,phone_number=None):
-    payload = {}
+def resolve_update_user(
+    obj,
+    resolve_info: GraphQLResolveInfo,
+    id,
+    name=None,
+    username=None,
+    password=None,
+    email=None,
+    admin=None,
+    phone_number=None,
+    ad=None
+):
+    payload = dict()
+    if id is not None:
+        payload["id"]=id
     if name is not None:
-        payload["name"] = name
+        payload["name"]=name
     if username is not None:
-        payload["username"] = username
+        payload["username"]=username
     if password is not None:
-        payload["password"] = password
+        payload["password"]=password
     if email is not None:
-        payload["email"] = email
+        payload["email"]=email
     if admin is not None:
-        payload["admin"] = admin
+        payload["admin"]=admin
     if phone_number is not None:
-        payload["phone_number"] = phone_number
+        payload["phone_number"]=phone_number
+    if ad is not None:
+        payload["ad"]=ad
+    return requests.patch(f"http://tarea_u4_service_users/users/{id}",json=payload).json()
 
-    return requests.put(f"http://tarea_u4_service_users/users/{id}", json=payload)
+@mutation.field("deleteUser")
+def resolve_delete_user(
+    obj,
+    resolve_info: GraphQLResolveInfo,
+    id
+):
+    return requests.delete(f"http://tarea_u4_service_users/users/{id}").json()
 
-#@mutation.field("deleteUser")
-#def resolve_delete_user(obj, resolve_info: GraphQLResolveInfo,id):
- #   user_data=mongodb_client.users.users.find_one({""})
+@mutation.field("updateUserAd")
+def resolve_update_user_ad(
+    obj,
+    resolve_info: GraphQLResolveInfo,
+    id,
+    ad
+):
+    return requests.put(f"http://tarea_u4_service_users/users/{id}/{ad}").json()
 
-  #  return requests.post(f"http://tarea_u4_service_users/users", json=payload).json()
+@mutation.field("deleteUserAd")
+def resolve_delete_user_ad(
+    obj,
+    resolve_info: GraphQLResolveInfo,
+    id
+):
+    return requests.delete(f"http://tarea_u4_service_users/users/{id}/ad").json()
 
 schema = make_executable_schema(type_defs, query, mutation, user)
-app = CORSMiddleware(GraphQL(schema, debug=True), allow_origins=['*'], allow_methods=("GET", "POST", "OPTIONS"))
+app = CORSMiddleware(
+    GraphQL(schema, debug=True),
+    allow_origins=["*"],
+    allow_methods=("GET", "POST", "OPTIONS"),
+)
